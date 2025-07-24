84b686ec3264d732f480e3874d8a267609a1a25a
kangod3 <minkyo84@gmail.com>
2025-07-24 19:32:46 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,10 +73,6 @@ variable "key_name" {
   type = string
 }
 
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
 resource "aws_instance" "monitoring_ec2" {
   ami                         = "ami-0c9c942bd7bf113a2"  # Ubuntu 22.04 (서울 리전)
   instance_type               = "t2.micro"
