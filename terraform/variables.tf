13b93dbed3f9c46acedd4f9ee5ac27f6fe13d998
kangod3 <minkyo84@gmail.com>
2025-07-24 19:54:22 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  description = "Your AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "Your AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
-
 variable "key_name" {
   description = "Name of the AWS Key Pair to use"
   type        = string
