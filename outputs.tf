5c170905e96ffe8e481df9f60b1faaf28ad87bd1
kangod3 <minkyo84@gmail.com>
2025-07-24 19:09:58 +0900

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.bpf_detector.id
-}
-
-output "public_ip" {
-  description = "Public IP address of the instance"
-  value       = aws_instance.bpf_detector.public_ip
-}
